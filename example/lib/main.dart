be9736c24934ccec0a3d53bf4dae82a5b9d36f98
remon <blackshadowsoftwareltd@gmail.com>
2022-12-21 00:38:54 +0600

    useing riverpod

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,53 +1,5 @@
-// import 'package:animation_search_bar/animation_search_bar.dart';
-// import 'package:flutter/material.dart';
-
-// /// animation_search_bar
-// void main() =>
-//     runApp(const MaterialApp(home: Home(), debugShowCheckedModeBanner: false));
-
-// class Home extends StatefulWidget {
-//   const Home({Key? key}) : super(key: key);
-
-//   @override
-//   State<Home> createState() => _HomeState();
-// }
-
-// class _HomeState extends State<Home> {
-//   late TextEditingController controller;
-
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     controller = TextEditingController();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: PreferredSize(
-//             preferredSize: const Size(double.infinity, 65),
-//             child: SafeArea(
-//                 child: Container(
-//               decoration: const BoxDecoration(color: Colors.white, boxShadow: [
-//                 BoxShadow(
-//                     color: Colors.black26,
-//                     blurRadius: 5,
-//                     spreadRadius: 0,
-//                     offset: Offset(0, 5))
-//               ]),
-//               alignment: Alignment.center,
-//               child: AnimationSearchBar(
-//                   backIconColor: Colors.black,
-//                   centerTitle: 'App Title',
-//                   onChanged: (text) => debugPrint(text),
-//                   searchTextEditingController: controller,
-//                   horizontalPadding: 5),
-//             ))));
-//   }
-// }
-
-import 'package:animation_search_bar/animation_search_bar.dart';
+import 'package:animation_search_bar/animation_search_bar.dart'
+    show AnimationSearchBar;
 import 'package:flutter/material.dart';
 
 /// animation_search_bar
@@ -89,20 +41,20 @@ class _HomeState extends State<Home> {
               child: AnimationSearchBar(
                   backIconColor: Colors.black,
                   centerTitle: 'App Title',
-                  onChanged: (text) => setState(() {
-                        countries = _countries
-                            .where((e) =>
-                                e.toLowerCase().contains(text.toLowerCase()))
-                            .toList();
-                      }),
+                  onChanged: (text) {
+                    countries = _countries
+                        .where(
+                            (e) => e.toLowerCase().contains(text.toLowerCase()))
+                        .toList();
+                    setState(() {});
+                  },
                   searchTextEditingController: controller,
                   horizontalPadding: 5),
             ))),
         body: ListView.builder(
-          itemBuilder: (context, index) => ListTile(
-            title: Text(countries[index]),
-          ),
           itemCount: countries.length,
+          itemBuilder: (context, index) =>
+              ListTile(title: Text(countries[index])),
         ));
   }
 }
@@ -124,6 +76,5 @@ const _countries = [
   'Eritrea',
   'Ethiopia',
   'Gambia',
-  'Ghana',
-  'Grenada'
+  'Ghana'
 ];
